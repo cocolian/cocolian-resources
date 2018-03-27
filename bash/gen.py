8ab739361381d83ffbaed9099ffcced00e6f8dc5
shamphone <shamphone@gmail.com>
2018-03-27 12:35:20 +0800

    add images

--- bash/gen.py
+++ bash/gen.py
@@ -15,32 +15,76 @@ import codecs
 ## 导出路径
 root_path = u'D:/iphone/微信消息记录-李雄峰的 iPhone/'
 
-def gen_date_file(sub_path, target_folder, thedate) : 
-	## 寻找最近日期的xls文件
-	## 最新的文件路径
-	last_folder = u' '
-
-	for cur_folder in os.listdir(root_path):
-		if cmp(cur_folder, last_folder)>0 and  os.path.exists(root_path + cur_folder + sub_path):
-			last_folder = cur_folder
-		
-	unpath = root_path + last_folder + sub_path
-
-	print('使用最新文件: '+ unpath +'\n')
+def gen_date_file(source_path, sub_path, target_folder, thedate) : 
+	
 
 	## unpath = unicode(inpath, "utf8")
 
-	source=open(unpath)
+	source=open(source_path)
+	## 聊天内容总数；
+	line_count = 0; 
+	## 发言频率
+	freq ={}
+	## 计算聊天内容总数；
+	
+	for line in source:
+		if len(line)>690 and line[63:67] == 'xl24':
+			datetime=line[233:252]
+			date = line[233:243]
+			time = line[244:252]
+			
+			pos_end = line.find('</td>', 335)
+			name = line[335: pos_end].decode('gbk', errors='ignore').encode('utf-8')
+			
+
+			pos_start = line.find('x:str>', pos_end+5) + 6
+			pos_end = line.find('</td>', pos_start)
+			wechat_no = line[pos_start:pos_end]
+			
+			pos_start = line.find('x:str>', pos_end+5) + 6
+			pos_end = line.find('</td>', pos_start)
+			action = line[pos_start:pos_end].decode('gbk', errors='ignore').encode('utf-8')
+
+			pos_start = line.find('x:str>', pos_end+5) + 6
+			pos_end = line.find('</td>', pos_start)
+			msgtype = line[pos_start:pos_end].decode('gbk', errors='ignore').encode('utf-8')
+
+			pos_start = line.find('x:str>', pos_end+5) + 6
+			pos_end = line.find('</td>', pos_start)
+			msg = cgi.escape(line[pos_start:pos_end]).decode('gbk', errors='ignore').encode('utf-8')
+
+			if date == thedate and msgtype == u'文本':
+				line_count += 1
+				if freq.has_key(name) :
+					freq[name] += 1 
+				else :
+					freq[name] = 1				
+				
+	source.close()
+	
+	if line_count<2 :
+		return
+	
+	## 计算发言最多的人，作为嘉宾
+	pres_name = 'PaymentGroup'
+	pres_count = 0
+	for name in freq:
+		if freq[name]>pres_count :
+			pres_name = name
+			pres_count = freq[name]
+	
 	target_path = r'D:/cocolian/cocolian-docs/source/'+target_folder+'/_posts/'+thedate+'-chat.markdown'
 	target = codecs.open(target_path, 'w', encoding = 'utf-8', errors='ignore')
 	target.write(u'---\n')              
 	target.write(u'layout:     source \n')                        
 	target.write(u'title:      "'+thedate+'-WeChat"\n')
 	target.write(u'date:       '+ thedate+' 12:00:00\n')
-	target.write(u'author:     "PaymentGroup"\n')   
+	target.write(u'author:     '+ pres_name +'\n')
+	target.write(u'lines:      '+ str(line_count) +' \n') 
 	target.write(u'tag:		  [chat]\n')                        
 	target.write(u'---\n')              
 
+	source=open(source_path)
 
 	try:
 		for line in source:
@@ -121,12 +165,25 @@ def gen_date_file(sub_path, target_folder, thedate) :
 	
 def gen_files(sub_path, target_folder) :
 	## 默认导出最近10天的聊天记录
-	range = datetime.timedelta(days=25)
+	range = datetime.timedelta(days=100)
+
+## 寻找最近日期的xls文件
+	## 最新的文件路径
+	last_folder = u' '
+
+	for cur_folder in os.listdir(root_path):
+		if cmp(cur_folder, last_folder)>0 and  os.path.exists(root_path + cur_folder + sub_path):
+			last_folder = cur_folder
+		
+	unpath = root_path + last_folder + sub_path
+
+	print('使用最新文件: '+ unpath +'\n')
+
 
 	thedate = datetime.datetime.now().date() - range
 
 	while thedate < datetime.datetime.now().date() : 
-		gen_date_file(sub_path, target_folder, thedate.strftime("%Y-%m-%d"))
+		gen_date_file(unpath, sub_path, target_folder, thedate.strftime("%Y-%m-%d"))
 		thedate = thedate + datetime.timedelta(days = 1)
 	return
 ## 文件相对路径的位置
